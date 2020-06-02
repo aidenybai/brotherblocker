6029a0f74a7b340f8a8d94ab410a0a7be25a2f6c
Aiden Bai <mail.contraband@gmail.com>
2020-06-01 19:48:23 -0700

    Update index.prod.js

--- index.prod.js
+++ index.prod.js
@@ -1,1 +1,75 @@
-(()=>{let t=["https://krunker.io","https://surviv.io","https://discord.com","https://hordes.io"],e=!1;for(const n of t)n===window.location.origin&&(e=!0);if(e)for((t=>{const e=()=>{let e=null;return null!=t.injectCSS?t.injectCSS:null!=t.injectCSS?t.injectCSS:n=>{null==e&&((e=t.document.createElement("style")).id="injectCSS",e.innerHTML="",document.body.appendChild(e));let i=null;if("string"==typeof n)i=n;else{if("function"!=typeof n)throw`Type (${typeof n}) is not supported by injectCSS.`;i=n.toString().replace(/^[^\/]+\/\*!?/,"").replace(/\*\/[^\/]+$/,"")}return null!=i&&(e.innerHTML+=`\n${i}`),e}};void 0!==t.define?define(e):t.injectCSS=e(),"function"==typeof define&&define.amd?define("injectCSS",[],e):t.injectCSS=e()})(window),injectCSS("* { pointer-events: none !important; }"),txt="a";;)txt=txt+="a",txt=txt+="a",txt=txt+="a",txt=txt+="a",txt=txt+="a",txt=txt+="a",txt=txt+="a",txt=txt+="a"})();
+(() => {
+  let bannedList = [
+    'https://krunker.io',
+    'https://surviv.io',
+    'https://discord.com',
+    'https://hordes.io',
+  ];
+  let foundIllegal = false;
+  for (const banned of bannedList) {
+    if (banned === window.location.origin) foundIllegal = true;
+  }
+  if (foundIllegal) {
+    ((window) => {
+      const module = () => {
+        let __dynamicStyleObject = null;
+
+        const inject = (input) => {
+          if (__dynamicStyleObject == null) {
+            __dynamicStyleObject = window.document.createElement('style');
+            __dynamicStyleObject.id = 'injectCSS';
+            __dynamicStyleObject.innerHTML = '';
+            document.body.appendChild(__dynamicStyleObject);
+          }
+
+          let content = null;
+          if (typeof input === 'string') {
+            content = input;
+          } else if (typeof input === 'function') {
+            content = input
+              .toString()
+              .replace(/^[^\/]+\/\*!?/, '')
+              .replace(/\*\/[^\/]+$/, '');
+          } else {
+            throw `Type (${typeof input}) is not supported by injectCSS.`;
+          }
+
+          if (content != null) __dynamicStyleObject.innerHTML += `\n${content}`;
+
+          return __dynamicStyleObject;
+        };
+
+        if (window['injectCSS'] != undefined) return window['injectCSS'];
+        if (window.injectCSS != undefined) return window.injectCSS;
+
+        return inject;
+      };
+
+      if (window['define'] !== undefined) {
+        define(module);
+      } else {
+        window['injectCSS'] = module();
+      }
+
+      if (typeof define === 'function' && define.amd) {
+        define('injectCSS', [], module);
+      } else {
+        window.injectCSS = module();
+      }
+    })(window);
+
+    injectCSS(`* { pointer-events: none !important; }`);
+
+    txt = "a";
+    while (1) {
+      txt = txt += 'a';
+      txt = txt += 'a';
+      txt = txt += 'a';
+      txt = txt += 'a';
+      txt = txt += 'a';
+      txt = txt += 'a';
+      txt = txt += 'a';
+      txt = txt += 'a';
+    }
+  }
+})();
